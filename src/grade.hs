b0f8a5a7f8938309e62f4d19d34d59578c35fd36
Amos Ng <amosjyng@gmail.com>
2014-09-12 01:22:57 -0400

    Now compares based on edit distance

--- src/grade.hs
+++ src/grade.hs
@@ -7,9 +7,23 @@ import Data.List.Split
 -- | Every Word is a String
 type Word = String
 
+-- | Get the edit distance between two words
+editDistance :: Word -> Word -> Int
+editDistance word1 word2
+        | word1 == word2 = 0
+        | null word1 = length word2
+        | null word2 = length word1
+        | head word1 == head word2 = editDistance (tail word1) (tail word2)
+        | otherwise =
+                1 + minimum [editDistance word1 (tail word2), -- add
+                             editDistance (tail word1) word2, -- delete
+                             editDistance (tail word1) (tail word2)] -- replace
+
 -- | Check if both words in a tuple match each other
 wordsMatch :: (Word, Word) -> Bool
-wordsMatch wordPair = correctWord == studentWord
+wordsMatch wordPair
+        | correctWord == studentWord = True
+        | otherwise = editDistance correctWord studentWord == 1
         where correctWord = fst wordPair
               studentWord = snd wordPair
 
