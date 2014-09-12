3b743354a60d9aab8f3176065b66f9fc35aa13be
Amos Ng <amosjyng@gmail.com>
2014-09-12 00:44:41 -0400

    Does a rudimentary word-by-word match

--- src/grade.hs
+++ src/grade.hs
@@ -2,8 +2,22 @@ import System.IO
 import System.Environment
 import Data.List.Split
 
+-- DATA STRUCTURES
+
+-- | Every Word is a String
+type Word = String
+
+-- | Check if both words in a tuple match each other
+wordsMatch :: (Word, Word) -> Bool
+wordsMatch wordPair = correctWord == studentWord
+        where correctWord = fst wordPair
+              studentWord = snd wordPair
+
+-- | From commandline arguments, see if the student answer reasonably matches
+-- the expected correct answer
+main :: IO ()
 main = do
         args <- getArgs
-        let correctAnswer = head args
-        let studentAnswer = last args
-        putStrLn $ splitOn " " correctAnswer !! 0
+        let correctAnswer = splitOn " " $ head args
+        let studentAnswer = splitOn " " $ last args
+        print $ all wordsMatch $ zip correctAnswer studentAnswer
